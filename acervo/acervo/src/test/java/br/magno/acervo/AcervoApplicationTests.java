e3172427ee3f81b260c2e9f5718fdc633963c971
Magnorjunior <magno.mroj2004@gmail.com>
2025-03-24 16:28:06 -0300

    Add files via upload

---
+++ acervo/acervo/src/test/java/br/magno/acervo/AcervoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.magno.acervo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcervoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
